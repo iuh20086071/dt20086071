b97f0fe1c73219a14aa4ffd587ac48ed104cc709
clemence128 <baohung20020112@gmail.com>
2024-03-31 09:21:10 +0700

    Change X to Student id

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH20086071 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
-INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+INSERT INTO tblbookIUH20086071 (title, author) VALUES
+  ('DevOps', 'IUH20086071'),
+  ('Big Data', 'IUH20086071'),
+  ('Cloud Deployement', 'IUH20086071'),
+('Data Analysis', 'IUH20086071'),
+('Block Chain', 'IUH20086071');
